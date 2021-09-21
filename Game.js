100eefdb8e51e30e70c80a163cf15400f44e4f93
Imon-ai <79011568+Imon-ai@users.noreply.github.com>
2021-09-21 19:17:22 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,93 @@
+class Game {
+  constructor(){
+    
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      player1=new Character(400,500,20,20);
+      player2=new Character(800,500,20,20);
+      player1.display();
+      player2.display();
+      heros=[player1,player2]
+      
+      form = new Form();
+      form.display();
+    }
+   
+  
+  }
+
+  play(){
+    form.hide();
+    
+  
+    Player.getPlayerInfo();
+    form.titleHide();
+           
+
+    if(allPlayers !== undefined){
+
+      background(battleFieldBg);
+      var ground=new Ground(displayWidth/2-25,displayHeight/2+50,displayWidth-25,40);
+      ground.display();   
+      tint(0,ground.Visiblity);
+      var display_position = 0;
+     
+      var index = 0;
+      
+    var x =170;
+    var y;           
+    
+        for(var plr in allPlayers){
+                    
+           //add 1 to the index for every loop
+           index = index + 1 ;
+
+           //position the cars a little away from each other in x direction
+           x = x + 200;
+          heros[index -1].x = x;
+          heros[index - 1].y = y;
+            
+         
+          
+        if (plr === "player" + player.index){
+          fill("blue");
+          playerIndicate="player";
+        }else{
+          fill("red");
+          playerIndicate="opponent";
+        }
+        display_position+=20;
+        textSize(20);
+        text(playerIndicate+":"+allPlayers[plr].name ,70,display_position)
+      }
+   
+      
+
+    }
+  }
+  
+  
+  
+    }
